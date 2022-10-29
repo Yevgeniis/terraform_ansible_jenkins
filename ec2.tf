3f9d1c90c72ca32248fa92e7ee119db7e995db9b
Yevgenii <yevgenii@test.com>
2022-10-29 06:17:13 -0400

    attach SG to ec2

--- ec2.tf
+++ ec2.tf
@@ -16,7 +16,8 @@ resource "aws_instance" "infr_exr_ins-1" {
   ami           = "ami-0d593311db5abb72b"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.infr_exr.id
-  key_name      = "infr_exr_key"
+  vpc_security_group_ids = [aws_security_group.allow_basic_ports.id]
+  key_name      = module.key_pair.key_pair_name
   tags = {
     Terraform = "true"
     Name      = "infr_Instance-1"
@@ -29,7 +30,8 @@ resource "aws_instance" "infr_exr_ins-2" {
   ami           = "ami-0d593311db5abb72b"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.infr_exr.id
-  key_name      = "vockey"
+  vpc_security_group_ids = [aws_security_group.allow_basic_ports.id]
+  key_name      = module.key_pair.key_pair_name
   tags = {
     Terraform = "true"
     Name      = "infr_Instance-2"
