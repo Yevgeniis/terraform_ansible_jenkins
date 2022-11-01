eb207c523e3d508736d18688ff69c18fa18a1b33
Yevgenii <yevgenii@test.com>
2022-11-01 17:26:53 +0200

    add play book, edit seciruty and ec2

--- ec2.tf
+++ ec2.tf
@@ -40,5 +40,33 @@ resource "aws_instance" "infr_exr_ins-2" {
     Terraform = "true"
     Name      = "infr_Instance-2"
   }
+}
 
+
+resource "null_resource" "apply_ansible" {
+  depends_on = [aws_instance.infr_exr_ins-2,aws_volume_attachment.infr_ebs_ins_2_attach]
+  provisioner "local-exec" {
+        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key ~/.ssh/${aws_key_pair.infr_exr_key.key_name}.pem -i '${aws_instance.infr_exr_ins-2.public_ip},' playbook-master.yml"   
+  
+  }  
+}
+
+
+resource "aws_ebs_volume" "infr_ebs_ins_2" {
+  availability_zone = "${aws_instance.infr_exr_ins-2.availability_zone}"
+  size = 1
+  tags = {
+    Name = "infr_ebs_ins_2"
+    Terraform = true
+  }
+}
+
+
+
+resource "aws_volume_attachment" "infr_ebs_ins_2_attach" {
+  depends_on = [aws_ebs_volume.infr_ebs_ins_2]
+  device_name = "/dev/sdc"
+  volume_id = "${aws_ebs_volume.infr_ebs_ins_2.id}"
+  instance_id = "${aws_instance.infr_exr_ins-2.id}"
+  skip_destroy = true
 }
